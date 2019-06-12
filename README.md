7afb2483174ec252ba29f8e99529e6c39ecd3704
agasconl <alex@io.builders>
2019-06-12 12:39:07 +0200

    .

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # jwt.sh
 Bash Script to create JWT tokens with HS256 and RS256
+
+### Params:
+```
+ [jwt.sh]$ ./jwt.sh -h
+./jwt.sh Params:
+	-b | --b64url <value>		Convert <value> to base64Url and exit
+	-a | --algorithm <value>	Set the algorithm to be used. Valid values are:
+		* h | H | HMAC | HS256 -> for HMAC SHA256. This will require to set -S option
+		* r | R | RSA | RS256  -> for RSA SHA256. This will require to set -P option
+	-S | --secret <value>		Secret to be used with HS256
+	-P | --pub-key | --pub <file>	Path to file with the Private RSA to sign the token
+	-H | --header <value>		JSON value to set as header part of the JWT. If you don't specify this option, It will be generated automatically with -k | --kid value
+	-p | --payload <value>		JSON value to set as payload part of the JWT
+	-k | --kid <value>		Set the kid value to be used when autogenerating the header
+```
+
+### Usage:
+- Getting Base64Url result for given text
+```
+ [jwt.sh]$ ./jwt.sh -b "hello world"
+Base64Url:
+
+aGVsbG8gd29ybGQ
+
+ [jwt.sh]$
+```
+
+- Generating a HS256 JWT:
+```
+ [jwt.sh]$ ./jwt.sh -a HS256 -S my_secret -p '{"exp": "", "iat": "", "nbf": "", "sub": ""}'
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IiJ9.eyJleHAiOiIiLCJpYXQiOiIiLCJuYmYiOiIiLCJwb2wiOiIifQ.xJl5f5NuMTbITL9p3ahIiUtHCJPaAHw_NVyVPOfV4EM
+ [jwt.sh]$ 
+```
+
+- Generating a RS256 JWT:
+```
+ [jwt.sh]$ ./jwt.sh -a RS256 -P private.pem -p '{"exp": "", "iat": "", "nbf": "", "sub": ""}'
+eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IiJ9.eyJleHAiOiIiLCJpYXQiOiIiLCJuYmYiOiIiLCJzdWIiOiIifQ.SSMbloBUqr_gdKAZQKW-egu9hFEvOfUEpmR2resJCED6U5mr3gQIbrhlKpNGB1m8b82wemMa2WAELhCxvNmyXJ3G437JW6e5SqPPZ1KDNSmpmUpEJH2-9TW11fHxeoVGZteeVBnb12P6OCt87MYmxLAJdqxR1qVLGmpn4RIAkU4HQWWnkLbe5AMlEk_MUzws4oSJvvbLIdhV-FO_vNm5dmYwDKlc9DrL-NKDm2aBrbElg_89iy9KnU4VNVj28VA40SZKKlpBcPvYCLh8DUXCpkA_jAH0VOQliONdqEBpe1YclAJJoqoeamEEOQVwV6mGQjl5qYDPWEZDfnFTHKFkiyF-RhXsXZAvVlSq_0OqkPf5zKt2iZb94yy5KVN7n99Ag5lvBnShR1AiC-Ve2todJ0SLdtNFEHb_6iIkERXZE_AZkmVFXB5kacpzD4_A3XIUSqVJsDdajkwYu_I9Jxd3-LwaTtU9JT1XiX-9qFZthKzwyjecHK-MSBW5laR7VhN4iMejbERvcJYQGrp_dULKYwjzNJvnKrewqZwfzpOuEcrIWbMiJayoSmB37Lx1lEUa_mPwpjxCKVEHcI849tFbetP9N7wlrZ_HDHSRkweTE6QgnVK1ojLQhn3BTW2ej32guPk5yQPuKDjA9HySMX5OxkSXmsw7ls_V-q3CmWNWDAQ
+ [jwt.sh]$ 
+```
---
+++ jwt.sh
@@ -0,0 +1,96 @@
+#! /bin/bash
+# JWT encode and decode
+# Author: Alejandro Gascon
+# mail: gascon.leon@gmail.com
+
+ayuda() {
+	echo "$0 Params:"
+	echo -e "\t-b | --b64url <value>\t\tConvert <value> to base64Url and exit"
+	echo -e "\t-a | --algorithm <value>\tSet the algorithm to be used. Valid values are:"
+	echo -e "\t\t* h | H | HMAC | HS256 -> for HMAC SHA256. This will require to set -S option"
+	echo -e "\t\t* r | R | RSA | RS256  -> for RSA SHA256. This will require to set -P option"
+	echo -e "\t-S | --secret <value>\t\tSecret to be used with HS256"
+	echo -e "\t-P | --pub-key | --pub <file>\tPath to file with the Private RSA to sign the token"
+	echo -e "\t-H | --header <value>\t\tJSON value to set as header part of the JWT. If you don't specify this option, It will be generated automatically with -k | --kid value"
+	echo -e "\t-p | --payload <value>\t\tJSON value to set as payload part of the JWT"
+	echo -e "\t-k | --kid <value>\t\tSet the kid value to be used when autogenerating the header"
+}
+
+# Parsing params
+while (( $# )); do
+	case $1 in
+		-h | --help)
+			ayuda
+			exit 0;
+		;;
+		-b | --b64url)
+			shift;
+			echo -e "Base64Url:\n" && echo $(echo -n "$*" | openssl enc -base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n' ) && echo "";
+			exit 0;
+		;;
+		-k | --kid)
+			kid=$2;
+			shift;
+		;;
+		-a | --algorithm)
+			if [[ "$2" =~ h|H|HMAC|HS256 ]]; then
+				cipher="HS256";
+			else
+				if [[ "$2" =~ r|R|RSA|RS256 ]]; then
+					cipher="RS256";
+				else
+					echo -e "Cypher not known. Valid values are:\n\t - h | H | HMAC | HS256 -> for HS256\n\t - r | R | RSA | RS256 -> for RS256"
+					exit 1;
+				fi
+			fi
+			shift;
+		;;
+		-H | --header)
+		 	header=$(jq -c -r . <<< "$2" 2>&1);
+			[ $? -ne 0 ] && echo "--header param seems not to be a valid json. Error: $header" && exit 1;
+			header=$(echo -n "$header" | openssl enc -base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n' )
+			shift;
+		;;
+		-p | --payload)
+		 	payload=$(jq -c -r . <<< "$2" 2>&1);
+			[ $? -ne 0 ] && echo "--payload param seems not to be a valid json. Error: $payload" && exit 1;
+			payload=$(echo -n "$payload" | openssl enc -base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n' )
+			shift;
+		;;
+		-S | --secret)
+			secret="$2"
+			shift;
+		;;
+		-P | --pub-key | --pub)
+			[ ! -f $2 ] && echo "$2 is not a file" && exit 1
+			pub_key=$2
+			shift;
+		;;
+	esac;
+	shift;
+done
+[[ "$cipher" == "" ]] && echo "Please, set -a | --algorithm option to HS256 or RS256." && exit 2
+
+if [[ "$header" == "" ]]; then
+	header=$(jq -c -r . <<< "{\"alg\": \"$cipher\", \"typ\": \"JWT\", \"kid\": \"$kid\"}")
+	header=$(echo -n "$header" | openssl enc -base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n' )
+fi
+
+
+case $cipher in
+	HS256)
+		[[ "$secret" == "" ]] && echo "You need to set a secret with option -S" && exit 2
+		signature=$(echo -n "${header}.${payload}" | openssl dgst -sha256 -binary -hmac "${secret}" | openssl enc -base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n' )
+		[ $? -ne 0 ] && echo "Some problem occur when generating signature. Error: $signature" && exit 3
+	;;
+	RS256)
+		[[ "$pub_key" == "" ]] && echo "You need to set a secret with option -P" && exit 2
+		signature=$(echo -n "${header}.${payload}" | openssl dgst -sha256 -binary -sign ${pub_key} | openssl enc -base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n' )
+		[ $? -ne 0 ] && echo "Some problem occur when generating signature. Error: $signature" && exit 3
+	;;
+	*)
+		echo "What happended?? cipher equals to '$cipher'!!" && exit 10
+	;;
+esac
+
+echo "${header}.${payload}.${signature}"
